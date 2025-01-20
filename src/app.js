eee8d3b43302adc792a2c3734fa7c8d3a32527e3
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-01-20 14:43:02 +0530

    Update Code

--- src/app.js
+++ src/app.js
@@ -3,12 +3,15 @@ const app = express();
 const path = require("path");
 const indexRoutes = require("./routes/index.routes");
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.set("view engine", "ejs");
 app.set("views", "./src/views");
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "..", "public")));
-
 app.use("/", indexRoutes);
-
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
 module.exports = app;
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  image: {
+  username: {
     type: String,
     required: true,
+    unique: true,
   },
-  name: {
+  profileImage: {
     type: String,
     required: true,
   },
@@ -16,14 +17,16 @@ const userSchema = new mongoose.Schema({
   },
   bio: {
     type: String,
-    required: true,
   },
-  password: {
+  gender: {
     type: String,
-    required: true,
+    enum: ["male", "female", "other"],
+    default: "other",
+  },
+  age: {
+    type: Number,
   },
 });
 
 const User = mongoose.model("User", userSchema);
-
 module.exports = User;
